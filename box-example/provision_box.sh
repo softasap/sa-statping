0d4428cd56cc6517aa0a3bd925340fff4925855a
Vyacheslav Voronenko <git@voronenko.info>
2019-03-16 16:15:43 +0100

    improve(box-example) added box-example

---
+++ box-example/provision_box.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Static parameters
+
+REMOTE_HOST=174.138.4.52
+BOX_DEPLOY_USER=root
+
+WORKSPACE=$(
+  cd $(dirname "$0")
+  pwd
+)
+
+BOX_PLAYBOOK=$WORKSPACE/box.yml
+BOX_NAME=test_box
+BOX_ADDRESS=$REMOTE_HOST
+BOX_USER=$BOX_DEPLOY_USER
+BOX_PWD=$BOX_DEPLOY_PASS
+BOX_PROVIDER=${BOX_PROVIDER:-}
+ENVIRONMENT=${ENVIRONMENT:-default}
+
+
+prudentia ssh <<EOF
+unregister $BOX_NAME
+
+register
+$BOX_PLAYBOOK
+$BOX_NAME
+$BOX_ADDRESS
+$BOX_USER
+$BOX_PWD
+
+verbose 1
+set box_address $BOX_ADDRESS
+set ansible_become_password $BOX_PWD
+set box_provider $BOX_PROVIDER
+set env $ENVIRONMENT
+
+provision $BOX_NAME
+EOF
